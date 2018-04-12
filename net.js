0764eba09565919470cd6854a7b0e58e732b4cda
Nicholas Choi <choinic1@cslinux.utm.utoronto.ca>
2018-04-11 23:58:58 -0400

    Kind of working web sockets

--- net.js
+++ net.js
@@ -13,14 +13,54 @@ wss.on('close', function() {
     console.log('disconnected');
 });
 
-/*
-wss.broadcast = function(){
-        for(let ws of this.clients){
-                ws.send(JSON.stringify(votes));
-        }
-} */
+wss.broadcast = function(data){
+    for (let ws of this.clients){
+    	ws.send(JSON.stringify(data));
+    }
+}
 
 wss.on('connection', function(ws) {
+
+	// On initial connection.
     console.log("A new client connected!");
+    ws.send(JSON.stringify({'board': board}));
+
+    ws.on('message', function(event) {
+    	var jsonData = JSON.parse(event);
+    	clickSquare(jsonData.move);
+    	ws.send(JSON.stringify({'board': board}));
+    });
 }); 
 
+
+function clickSquare(i){
+	console.log(""+i);
+	if(move(i,i+board[i]))return;
+	move(i,i+2*board[i]);
+
+	// isWon();
+}
+
+/* Return true if moved, false otherwise. */
+function move(i,j){
+	if (isEmpty(j) == true) {
+		var temp = board[i];
+		board[i] = board[j];
+		board[j] = temp;
+		wss.broadcast(JSON.stringify({'board': board}))
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+/* Return true if board tile is 0 (empty), false otherwise. */
+function isEmpty(i){
+	if (board[i] == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
